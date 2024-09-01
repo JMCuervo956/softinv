a50ce73f1d750b1fea4d90882756913644ea6af9
Jose M Cuervo <josmansoft24@gmail.com>
2024-09-01 18:28:37 -0500

    first commit

--- src/app.js
+++ src/app.js
@@ -1,48 +1,31 @@
+//require('dotenv').config(); 
 
 // 1. - Invocamos a express
 import express from 'express';
 import {pool} from './db.js';
 import {PORT} from './config.js';
-import mysql from 'mysql2';
+import mysql from 'mysql2/promise'; // Cambiado para usar mysql2 con promesas
  
 const app = express()
 
-import http from 'http';
+// Configura el motor de plantillas EJS
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
-// Crear el servidor y asignarlo a la variable 'server'
-
-app.get('/', async(req, res)=>{
-    const rows = await pool.query("select * from preguntas")
-    res.json(rows)
-})
-
-/*
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    //res.end('Hello World\n');
+// Ruta para la página principal
+app.get('/', async (req, res) => {
+    try {
+        // Ejecuta la consulta a la base de datos
+        const [rows] = await pool.execute("SELECT * FROM preguntas");
+        // Renderiza la plantilla 'index.ejs' pasando los resultados de la consulta
+        res.render('index', { preguntas: rows });
+    } catch (error) {
+        console.error('Error al ejecutar la consulta:', error);
+        res.status(500).json({ error: 'Error interno del servidor' });
+    }
 });
-*/
 
-
-// Hacer que el servidor escuche en el puerto 3000
-
-/*
-server.listen(3000, '127.0.0.1', () => {
-    console.log('Servidor corriendo en.................. http://127.0.0.1:/', 3000);
+app.listen(PORT, () => {
+    console.log('Server en port', PORT);
 });
-*/
- 
-
-/*
-app.get('/', async(req, res)=>{
-    const rows = await pool.query("select * from preguntas")
-    res.json(rows)
-})
-*/
- 
-////////////////////////////////////////////////////////
-
-app.listen(PORT)
-console.log('Server en port', PORT) 
-
+  
