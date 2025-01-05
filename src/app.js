c1c0132fca019ac0ff75fbb258261d6372372556
Jose M Cuervo <josmansoft24@gmail.com>
2025-01-05 17:56:38 -0500

    first commit

--- src/app.js
+++ src/app.js
@@ -183,7 +183,7 @@ function showAlert() {
 
 app.get('/', async (req, res) => {		
     try {		
-        res.render('login');		
+        res.render('inventarios');		
     } catch (error) {		
         console.error('Error al renderizar la plantilla:', error);		
         res.status(500).json({ error: 'Error interno del servidor' });		
